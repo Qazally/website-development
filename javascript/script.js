ae42927a6aeb9092ae19da5baceb8d71cc14b353
Qazllay <22014156-039@uog.edu.pk>
2025-08-07 12:32:58 +0500

    js started

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+// browser file 
+//because alert only works in the browser not in the node js
+
+
+alert("hi")
+console.log("code is running...");
+console.log("code is looking like a wow...");
+
+var a =prompt ("Wnter your number")
+var istrue = confirm("is your computer blast")
+if(istrue){
+    console.log("your computer is blast")
+}
+else{
+    console.log("your computer is not blast")
+}
