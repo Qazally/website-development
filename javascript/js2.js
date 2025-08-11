dcea673a1741a248a399cbea92ee2d08746b622d
Qazllay <22014156-039@uog.edu.pk>
2025-08-11 16:37:32 +0500

    Strings in JS

--- javascript/js2.js
+++ javascript/js2.js
@@ -71,7 +71,9 @@ if (random > 0.1) {
 */
 
 
-//STRINGS-------------------------------------------------------------------------------------------------------------
+//STRINGS---------------------------------------------------------------------------------------------------------------
+
+
  //returning charachters of string
 
  let a = "Qazal";
@@ -95,9 +97,50 @@ if (random > 0.1) {
 
  //Escape sequence character
 
- console.log("Line1\nLine2");
+console.log("Line1\nLine2");
 console.log("Tab\tSpace");
 console.log("She said: \"You're awesome!\"");
 console.log("Path: C:\\Users\\Qazal");
 
 //String properties or methods:
+ 
+
+//upper case and lower case
+console.log(real_name.toUpperCase());//QAZAL
+console.log(real_name.toLowerCase());//qazal
+
+// //length
+console.log(real_name.length); //5
+
+// //slice()
+console.log(real_name.slice(1, 3)); //az (1 included, 3 not included)
+console.log(real_name.slice(1)); //azal (starting from 1 to the end of the string )
+
+// //replace()
+console.log(real_name.replace("al", "334")); //Qazal
+
+// //concat()
+console.log(real_name.concat(" ", friend)); //qazal zahra
+console.log(real_name.concat(" ", friend, " ayesha")); //qazal zahra ayesha
+
+//trim
+let b="   Ayesha     "
+console.log(b.trim()); //Ayesha (removes whitespaces)
+
+//charAt(index)
+console.log(real_name.charAt(0)); //q
+//charCodeAt(index)
+console.log(real_name.charCodeAt(0)); //113 
+
+//indexOf()
+console.log(real_name.indexOf("a")); //1
+//lastIndexOf()
+console.log(real_name.lastIndexOf("a")); //3
+//localeCompare()
+console.log(real_name.localeCompare("qazal")); //0
+//match()
+console.log(real_name.match(/a/)); //["a", index: 2, input:
+//search()
+console.log(real_name.search(/a/)); //1
+
+
